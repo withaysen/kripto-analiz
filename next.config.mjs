ae8641bdb8364e3e91ab76d2247b42566d2228f4
withaysenaysenur.alahabali@std.yildiz.edu.tr <mail@adresiniz.com>
2025-12-19 15:06:40 +0300

    Static export ayari

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
